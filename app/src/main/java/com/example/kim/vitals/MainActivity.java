b05f84adb95fa32af6bd53659fcef7d555311a8b
EDWARD KASIBANTE JB <edwardkasibante@gmail.com>
2021-08-18 14:48:20 +0300

    initial Commit

--- app/src/main/java/com/example/kim/vitals/MainActivity.java
+++ app/src/main/java/com/example/kim/vitals/MainActivity.java
@@ -2,14 +2,15 @@ package com.example.kim.vitals;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity {
 
     Button temp,hrate,oxygen,pressure;
 
